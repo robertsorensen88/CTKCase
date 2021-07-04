e2edced7cb500a15c6d49920c42ca8c9f844b6ee
Robert Sörensen <robert.sorensen44@gmail.com>
2021-07-04 14:25:11 +0200

    Gets only values at 12:00:00 for forecast

--- javascript/main.js
+++ javascript/main.js
@@ -21,35 +21,46 @@ searchButton.addEventListener("click" , function(){
         removeElement();
 
         let cityName = data["name"];
+        let country = data["sys"]["country"]
         let currentTemperature = parseInt(data["main"]["temp"]);
+        let currentFeelsLike = parseInt(data["main"]["feels_like"]);
         let weatherDescription = data["weather"][0]["description"];
         let icon = data["weather"][0]["icon"];
         let iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
-        let longitude = data["coord"]["lon"];
-        let latitude = data["coord"]["lat"];
 
         let weatherHeader = document.createElement("h1");
-        let temp = document.createElement("p");
-        let desc = document.createElement("p");
+        let secondHeader = document.createElement("h3");
+        let temperature = document.createElement("p");
+        let feelsLike = document.createElement("p");
+        let description = document.createElement("p");
         let imgicon = document.createElement("img");
         let weatherOutput = document.createElement("div");
+        let feelsLikeHeader = document.createElement("h5");
 
         weatherHeader.id = "hId";
-        weatherHeader.innerHTML = "Current weather in " + cityName;
+        weatherHeader.innerHTML = cityName + ", " + country;
         weatherOutput.id = "weatherOutput";
         weatherOutput.className =
-          "d-flex mw-50 flex-column justify-content-center border border-primary rounded";
-        temp.id = "tId";
-        temp.innerHTML = currentTemperature + " °C";
-        desc.id = "dId";
-        desc.innerHTML = weatherDescription;
+          "d-flex mw-50 flex-column justify-content-center border border-dark border-3 rounded";
+        feelsLikeHeader.innerHTML= "Feels like";
+        feelsLikeHeader.classList= "border-top border-dark pt-3"
+        
+        feelsLike.innerHTML = currentFeelsLike + " °C";
+        feelsLike.classList = "h4";
+        temperature.id = "tId";
+        temperature.className ="h3";
+        temperature.innerHTML = currentTemperature + " °C";
+        description.id = "dId";
+        description.innerHTML = weatherDescription;
         imgicon.id = "iID";
         imgicon.src = iconUrl;
 
         contentfield.appendChild(weatherHeader)
         weatherOutput.appendChild(imgicon)
-        weatherOutput.appendChild(temp);
-        weatherOutput.appendChild(desc);
+        weatherOutput.appendChild(temperature);
+        weatherOutput.appendChild(description);
+        weatherOutput.appendChild(feelsLikeHeader);
+        weatherOutput.appendChild(feelsLike);
         contentfield.appendChild(weatherOutput);
     })
     .catch((error) => alert("There is no city like that!"))
@@ -66,42 +77,27 @@ searchButton.addEventListener("click" , function(){
       console.log(data["list"][16]["dt_txt"]);
       console.log(data["list"][24]["dt_txt"]);
       console.log(data["list"][32]["dt_txt"]);
-      let weatherForecastFiveDaysDates = []; 
 
+      let weatherForecastFiveDaysDates = []; 
+      let weatherForecastFiveDaysTemperature = [];
+      let weatherForecastForecastIcon = [];
+      let weatherForecastDescription = [];
+
+
+      // Get values at 12:00:00
       for (let index = 0; index < data["list"].length; index++) {
        let splitDate = data["list"][index]["dt_txt"].split(" ")
         console.log(splitDate[1])
           if (splitDate[1] == "12:00:00") {
           weatherForecastFiveDaysDates.push(data["list"][index]["dt_txt"]);
-          }
-       
+          weatherForecastFiveDaysTemperature.push(
+            data["list"][index]["main"]["temp"]
+          );
+          weatherForecastForecastIcon.push(data["list"][index]["weather"][0]["icon"]);
+          weatherForecastDescription.push(data["list"][index]["weather"][0]["description"]);
+          }      
       }
-      for (let index = 0; index < weatherForecastFiveDaysDates.length; index++) {
-        console.log(weatherForecastFiveDaysDates[index])
-        
-      }
-     
-      const weatherForecastFiveDaysTemperature = [
-        data["list"][0]["main"]["temp"],
-        data["list"][8]["main"]["temp"],
-        data["list"][16]["main"]["temp"],
-        data["list"][24]["main"]["temp"],
-        data["list"][32]["main"]["temp"],
-      ];
-      const weatherForecastForecastIcon = [
-        data["list"][0]["weather"][0]["icon"],
-        data["list"][8]["weather"][0]["icon"],
-        data["list"][16]["weather"][0]["icon"],
-        data["list"][24]["weather"][0]["icon"],
-        data["list"][32]["weather"][0]["icon"],
-      ];
-      const weatherForecastDescription = [
-        data["list"][0]["weather"][0]["description"],
-        data["list"][8]["weather"][0]["description"],
-        data["list"][16]["weather"][0]["description"],
-        data["list"][24]["weather"][0]["icon"],
-        data["list"][32]["weather"][0]["icon"],
-      ];
+
       let forecastHeader = document.createElement("h3");
       forecastHeader.innerHTML= "Five days forecast";
       forecastHeader.id ="forecastHeader";
@@ -114,16 +110,19 @@ searchButton.addEventListener("click" , function(){
         const getWeatherForecastTemperature = weatherForecastFiveDaysTemperature[index];
         let forecastOutput = document.createElement("div");
         let forecastTemperature = document.createElement("p");
+        let forecastDescription = document.createElement("p");
         let forecastDate = document.createElement("h6");
         let forecastIcon = document.createElement("img");
 
         let getDate = getWeatherForecastDate.split(" ");
 
+        forecastDescription.innerHTML = weatherForecastDescription[index]
         forecastIcon.src = "http://openweathermap.org/img/w/" + weatherForecastForecastIcon[index] + ".png";
         forecastIcon.id ="iID";
         forecastOutput.id = "forecastWindows";
         forecastOutput.className =
-          "m-4 d-flex mw-50 flex-column justify-content-center border border-primary rounded";
+          "mb-4 d-flex mw-50 flex-column justify-content-center border border-dark border-3 rounded";
+        forecastTemperature.classList="h3";
         forecastTemperature.innerHTML =
           parseInt(getWeatherForecastTemperature) + " °C";
         forecastDate.innerHTML = getDate[0];
@@ -132,6 +131,7 @@ searchButton.addEventListener("click" , function(){
         contentfield.appendChild(forecastDate);
         forecastOutput.appendChild(forecastIcon);
         forecastOutput.appendChild(forecastTemperature);
+        forecastOutput.appendChild(forecastDescription);
         contentfield.appendChild(forecastOutput);
       }
      
